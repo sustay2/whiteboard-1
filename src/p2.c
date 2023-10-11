2732cab816e89419a2ba71c222f6ad69036cb72d
Sim Yong Xiang <145644886+sustay2@users.noreply.github.com>
2023-10-11 14:08:42 +0800

    Copied P2 code from IDE to p2.c

--- src/p2.c
+++ src/p2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void main() {
+    for (int i=1; i<=100; i++) {            // using the for loop so it stops at 100 
+        if (i%3==0 && i%5==0) {             // using modulus to get the perfect divisible number
+            printf("FizzBuzz");
+        } else if (i%3==0) {
+            printf("Fizz");
+        } else if (i%5==0) {
+            printf("Buzz");
+        } else {
+            printf("%d", i);
+        }
+
+        if (i!=100) {
+            printf(", ");       // print comma when it's not 100
+        } else {
+            printf("\n");       // go to the next line when it reaches 100
+        }
+    }
+}
