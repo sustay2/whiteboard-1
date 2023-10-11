e856fd6c4993f418bd92dccc989305a51cbfe2bb
Sim Yong Xiang <145644886+sustay2@users.noreply.github.com>
2023-10-11 14:22:56 +0800

    Copied P7 code from IDE to p7.c

--- src/p7.c
+++ src/p7.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int sqroot(int x) {
+    if (x == 0 || x == 1) {
+        return x;                               // return 0 or 1 if x is equal to 0 or 1
+    }
+
+    if (x < 0) {
+        return -1;                              // terminate the func if x is a negative num
+    }
+
+    int sqrt_x = 0;
+
+    while (sqrt_x * sqrt_x < x) {               // multiply sqrt_x until it is bigger than x
+        sqrt_x++;                               // plus 1 to sqrt_x after multiplied value is smaller than x
+    }
+
+    return sqrt_x;
+}
+
+int main() {
+    int x = 36;                                 // using 36, as a perfect sqrtroot
+
+    int squareRoot = sqroot(x);
+
+    printf("Square root of %d is %d\n", x, squareRoot);
+
+    return 0;
+}
